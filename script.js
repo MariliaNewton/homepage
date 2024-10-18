49a3b8aa8af97fba6a14b7f9757021f5fca85689
marilianewton <mariliagomesnewton@gmail.com>
2024-10-18 13:58:11 +0200

    Simplify card animation

--- script.js
+++
@@ -1,9 +0,0 @@
-// const test = document.querySelector(".photo-container");
-const cardAnimation = document.querySelector(".container-animation");
-
-cardAnimation.addEventListener("mouseleave", () => {
-  cardAnimation.classList.add("revert");
-});
-cardAnimation.addEventListener("mouseenter", () => {
-  cardAnimation.classList.remove("revert");
-});
